ae57992e8967b65e084f8fe52b1709e9c8e35a16
denis122 <dmenyhov@yahoo.com>
2021-02-03 17:56:49 +0200

    Lesson 2.1 вывели действительно 2 точки

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -11,13 +11,24 @@ namespace Tetris
 
             int x1 = 2;
             int y1 = 3;
-            //char ch1 = '*';
+            char ch1 = '*';
 
-            Console.SetCursorPosition(x1,y1);
-            //Console.WriteLine(ch1);
+            Draw(x1, y1, ch1);
+
+            int x2 = 3;
+            int y2 = 4;
+            char ch2 = '#';
+
+            Draw(x2, y2, ch2);
 
             Console.ReadLine();
 
+        }
+
+        static void Draw(int x, int y, char ch)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(ch);
         }
     }
 }
